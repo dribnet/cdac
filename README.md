28a1fb622327e9ed1cad0ae3b73329c2b4e5816d
Tom White <tom@sixdozen.com>
2013-04-07 00:16:18 -0700

    chapter 1: code and Drakefile through p21
    Running through the examples in the repl. Also adding
    drake files to manage dependencies and downloads.

---
+++ README.md
@@ -0,0 +1,18 @@
+# CDAC
+
+My own code from Clojure Data Analysis Cookbook
+
+Variations of code from the book + Drakefiles
+
+(each chapter in a separate project)
+
+## Usage
+
+lein repl
+(p##)
+
+## License
+
+Distributed under the WTFPL.
+
+http://www.wtfpl.net/txt/copying/
---
+++ ch1/src/ch1/importing.clj
@@ -0,0 +1,105 @@
+(ns ch1.importing
+  (:require [incanter.core :refer [to-dataset dataset]]
+            [incanter.io :refer [read-dataset]]
+            [incanter.excel :refer [read-xls]]
+            [net.cgrand.enlive-html :as html]
+            [clojure.java.jdbc :refer [with-connection with-query-results]]
+            [clojure.xml :refer [parse]]
+            [clojure.string :as string]
+            [clojure.zip :refer [children down right xml-zip]]
+            [clojure.data.json :refer [read-str]])
+  (:import  [java.net URL]))
+
+(defn p10
+  "print results of read-dataset :header true"
+  []
+  (println "=== read-dataset ===")
+  (print (read-dataset "data/small-sample.csv"))
+  (println "=== read-dataset :header true ===")
+  (print (read-dataset "data/small-sample.csv" :header true)))
+
+(defn p12
+  "print results of to-dataset json/read-str"
+  []
+  (println "=== to-dataset json/read-str ===")
+  (print (to-dataset (read-str (slurp "data/small-sample.json")))))
+
+(defn p13
+  "print results of read-xls"
+  []
+  (println "=== read-xls ===")
+  (print (read-xls "data/small-sample-header.xls")))
+
+(def db {:subprotocol "sqlite"
+         :subname "data/small-sample.sqlite"
+         :classname "org.sqlite.JDBC"})
+
+(defn load-table-data
+  "This loads the data from a database table."
+  [db table-name]
+  (let [sql (str "SELECT * FROM "
+                 table-name ";")]
+    (with-connection db
+      (with-query-results rs [sql]
+        (to-dataset (doall rs))))))
+
+(defn p15
+  "print results of reading clojure.java.jdbc data"
+  []
+  (println "=== read via jdbc ===")
+  (print (load-table-data db "people")))
+
+(defn load-xml-data [xml-file first-data next-data]
+  (let [data-map (fn [node]
+                   [(:tag node) (first (:content node))])]
+    (->>
+      ;; 1. Parse the XML data file;
+      (parse xml-file)
+      xml-zip
+      ;; 2. Walk it to extract the data nodes;
+      first-data
+      (iterate next-data)
+      (take-while #(not (nil? %)))
+      (map children)
+      ;; 3. Convert them into a sequence of maps; and
+      (map #(mapcat data-map %))
+      (map #(apply array-map %))
+      ;; 4. Finally convert that into an Incanter dataset
+      to-dataset)))
+
+(defn p17
+  "print results of reading xml data"
+  []
+  (println "=== read via xml ===")
+  (print (load-xml-data "data/small-sample.xml" down right)))
+
+(defn to-keyword
+  "This takes a string and returns a normalized keyword."
+  [input]
+  (-> input
+    string/lower-case
+    (string/replace \space \-)
+    keyword))
+
+(defn load-data
+"This loads the data from a table at a URL."
+  [url]
+  (let [html (html/html-resource (URL. url))
+        table (html/select html [:table#data])
+        headers (->>
+                  (html/select table [:tr :th])
+                  (map html/text)
+                  (map to-keyword)
+                  vec)
+        rows (->> (html/select table [:tr])
+               (map #(html/select % [:td]))
+               (map #(map html/text %))
+               (filter seq))]
+    (dataset headers rows)))
+
+(defn p21
+  "print results of web scaping with enlive"
+  []
+  (println "=== read via live web scrape ===")
+  (print (load-data (str "http://www.ericrochester.com/"
+                         "clj-data-analysis/data/small-sample-table.html"))))
