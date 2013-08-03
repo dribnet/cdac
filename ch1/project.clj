d44738dc2abb12bf75ddc7344ffb20a463922e53
Tom White <tom@sixdozen.com>
2013-08-03 02:33:31 -0700

    ch1: updated dependencies and added ch1/p24 example
    Updated dependencies to latest version and added an
    extra cheshire dependency for example 12b.
    
    Also put in the more complicated enlive scraping example.

--- ch1/project.clj
+++ ch1/project.clj
@@ -4,11 +4,12 @@
   :license {:name "WTFPL"
             :url "http://www.wtfpl.net/txt/copying/"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/data.json "0.2.1"]
-                 [incanter/incanter-core "1.4.1"]
-                 [incanter/incanter-charts "1.4.1"]
-                 [incanter/incanter-excel "1.4.1"]
-                 [incanter/incanter-io "1.4.1"]
+                 [org.clojure/data.json "0.2.2"]
+                 [cheshire "5.2.0"]
+                 [incanter/incanter-core "1.5.1"]
+                 [incanter/incanter-charts "1.5.1"]
+                 [incanter/incanter-excel "1.5.1"]
+                 [incanter/incanter-io "1.5.1"]
                  [org.clojure/java.jdbc "0.2.3"]
                  [enlive "1.1.1"]
                  [org.xerial/sqlite-jdbc "3.7.2"]]
--- ch1/src/ch1/importing.clj
+++ ch1/src/ch1/importing.clj
@@ -1,3 +1,7 @@
+; REPL: (require `ch1.importing :verbose :reload)
+;       (ch1.importing/p10)
+;       etc.
+
 (ns ch1.importing
   (:require [incanter.core :refer [to-dataset dataset]]
             [incanter.io :refer [read-dataset]]
@@ -7,7 +11,8 @@
             [clojure.xml :refer [parse]]
             [clojure.string :as string]
             [clojure.zip :refer [children down right xml-zip]]
-            [clojure.data.json :refer [read-str]])
+            [clojure.data.json :refer [read-str]]
+            [cheshire.core :refer [parse-string]])
   (:import  [java.net URL]))
 
 (defn p10
@@ -23,6 +28,12 @@
   []
   (println "=== to-dataset json/read-str ===")
   (print (to-dataset (read-str (slurp "data/small-sample.json")))))
+
+(defn p12b
+  "print results of to-dataset cheshire.core/parse-string"
+  []
+  (println "=== to-dataset cheshire.core/parse-string ===")
+  (print (to-dataset (parse-string (slurp "data/small-sample.json")))))
 
 (defn p13
   "print results of read-xls"
@@ -103,3 +114,35 @@
   (println "=== read via live web scrape ===")
   (print (load-data (str "http://www.ericrochester.com/"
                          "clj-data-analysis/data/small-sample-table.html"))))
+
+(defn get-family
+  "This takes an article element and returns the family name." 
+  [article]
+  (string/join (map html/text (html/select article [:header :h2]))))
+
+(defn get-person
+  "This takes a list item and returns a map of the persons' name and relationship."
+  [li]
+  (let [[{pnames :content} rel] (:content li)]
+    {:name (apply str pnames)
+     :relationship (string/trim rel)}))
+
+(defn get-rows
+  "This takes an article and returns the person mappings, with the family name added."
+  [article]
+  (let [family (get-family article)]
+    (map #(assoc % :family family)
+         (map get-person (html/select article [:ul :li])))))
+
+(defn load-data "This downloads the HTML page and pulls the data out of it."
+  [html-url]
+  (let [html (html/html-resource (URL. html-url))
+        articles (html/select html [:article])]
+    (to-dataset (mapcat get-rows articles))))
+
+(defn p24
+  "print results of more advanced web scaping with enlive"
+  []
+  (println "=== read via live web scrape ===")
+  (load-data (str "http://www.ericrochester.com/"
+                  "clj-data-analysis/data/small-sample-list.html")))
